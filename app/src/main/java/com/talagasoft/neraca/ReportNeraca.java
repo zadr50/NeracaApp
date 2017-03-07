16bbb21c401fa4a6f583ddda1899901fc9b3ab9c
Andri Andriana <andri@talagasoft.com>
2017-03-07 22:20:54 +0700

    Update Terbaru
    Update Terbaru

--- app/src/main/java/com/talagasoft/neraca/ReportNeraca.java
+++ app/src/main/java/com/talagasoft/neraca/ReportNeraca.java
@@ -1,8 +1,15 @@
 package com.talagasoft.neraca;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.talagasoft.model.ReportModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ReportNeraca extends AppCompatActivity   implements View.OnClickListener {
 
@@ -10,6 +17,17 @@ public class ReportNeraca extends AppCompatActivity   implements View.OnClickLis
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_neraca);
+
+        ReportModel rpt=new ReportModel();
+        ArrayList<HashMap<String, String>> recAccount = rpt.Neraca();
+
+        ListView lstAccount=(ListView) findViewById(R.id.list1);
+
+        final int[] toLayoutId = new int[] {R.id.tvAccount,R.id.tvDescription,R.id.tvBalance};
+        final String[] fromMapKey = new String[] {"account", "description","balance"};
+        SimpleAdapter adapterCoa = new SimpleAdapter(this,recAccount,R.layout.content_account_items,
+                fromMapKey,toLayoutId);
+        lstAccount.setAdapter(adapterCoa);
     }
     @Override
     public void onClick(View view) {
